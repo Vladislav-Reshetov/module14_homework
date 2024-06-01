04654a00ef24ebd6f2f82edab990fa7fbb866003
Vladislav-Reshetov <161346291+Vladislav-Reshetov@users.noreply.github.com>
2024-06-01 17:34:40 +0300

    Update script.js

--- Module14.Task 1-5/Task4/script.js
+++ Module14.Task 1-5/Task4/script.js
@@ -1,27 +1,24 @@
-const btn = document.querySelector("button");
-const resultNode = document.querySelector(".picture");
+const makeRequest = (valueWidth, valueHeight) => {
+    return fetch(`https://dummyimage.com/${valueWidth}x${valueHeight}/`)
+    .then((response) => { 
+        return response;
+    })
+    .catch(() => { 
+        document.querySelector('.picture').innerHTML = 'Error';
+        console.log('error');
+    });
+}
 
-const useRequest = (valueWidth, valueHeight) => {
-    return fetch('https://dummyimage.com/${valueWidth}x${valueHeight}')
-        .then((response) => {
-            displayResult(response.url);
-            return response;
-        })
-        .catch(() => { console.log ('error') })
-};
+let btn = document.querySelector('.button');
 
-function displayResult(linkSrc) {
-    let cards = '';
-    cards = cards + '<img class="image" src="${linkSrc}" alt="image">';
-    resultNode.innerHTML=cards;
-};
+btn.addEventListener('click', async () => {
+    const valueWidth = parseInt(document.querySelector('.input__width').value);
+    const valueHeight = parseInt(document.querySelector('.input__height').value);
+    if (!(100 <= valueWidth <= 300) || !(100 <= valueHeight <= 300)) {
+        document.querySelector('.picture').innerHTML = 'Одно из чисел вне диапазона от 100 до 300';
+    } else {
+        const requestResult = await makeRequest(valueWidth, valueHeight);
 
-btn.addEventListener("click", async () => {
-    const valueWidth = document.querySelector('.input__width').value;
-    const valueHeight = document.querySelector('.input__height').value;
-        if (valueWidth < 100 || valueWidth > 300 || valueHeight < 100 || valueHeight > 300 || isNaN(valueWidth) || isNaN(valueHeight)) {
-            resultNode.innerHTML = "Одно из чисел вне диапазона от 100 до 300";
-        } else {
-            const requestResult = await useRequest(valueWidth, valueHeight);
-        }
+        document.querySelector('.picture').innerHTML += `<img src="${requestResult.url}">`;  
+    }
 });
